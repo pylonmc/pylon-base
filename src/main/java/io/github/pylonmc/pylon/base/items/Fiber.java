979758b516e93235c68e120547021108073c0f5a
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-03-07 11:45:13 -0500

    Add 4 string -> fiber recipe

---
+++ src/main/java/io/github/pylonmc/pylon/base/items/Fiber.java
@@ -0,0 +1,32 @@
+package io.github.pylonmc.pylon.base.items;
+
+import io.github.pylonmc.pylon.core.item.ItemStackBuilder;
+import io.github.pylonmc.pylon.core.item.PylonItemSchema;
+import io.github.pylonmc.pylon.core.item.SimplePylonItem;
+import io.github.pylonmc.pylon.core.recipe.RecipeTypes;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.recipe.CraftingBookCategory;
+import org.jetbrains.annotations.NotNullByDefault;
+
+@SuppressWarnings("UnstableApiUsage")
+@NotNullByDefault
+public class Fiber extends PylonItemSchema {
+
+    public Fiber(NamespacedKey id, Class<? extends SimplePylonItem> itemClass, ItemStack template){
+        super(id, itemClass, template);
+        ShapedRecipe craftingRecipe = new ShapedRecipe(id, template);
+        craftingRecipe.shape(
+                "SS ",
+                "SS ",
+                "   "
+        );
+        craftingRecipe.setIngredient('S', new ItemStackBuilder(Material.STRING).build());
+        craftingRecipe.setCategory(CraftingBookCategory.MISC);
+        RecipeTypes.VANILLA_CRAFTING.addRecipe(craftingRecipe);
+    }
+
+    // need RECIPE_TYPE here because this is going to be used to make other items
+}
