8a22cf7e368798a484febe40b2afbd6c70f10362
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-03-15 01:21:25 -0400

    add ferroduralum item tier

--- src/main/java/io/github/pylonmc/pylon/base/PylonItems.java
+++ src/main/java/io/github/pylonmc/pylon/base/PylonItems.java
@@ -7,6 +7,7 @@ import io.github.pylonmc.pylon.core.item.ItemStackBuilder;
 import io.github.pylonmc.pylon.core.item.PylonItemSchema;
 import io.github.pylonmc.pylon.core.item.SimpleItemSchema;
 import io.github.pylonmc.pylon.core.item.SimplePylonItem;
+import io.github.pylonmc.pylon.core.recipe.RecipeTypes;
 import io.github.pylonmc.pylon.core.util.MiningLevel;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.Consumable;
@@ -17,8 +18,12 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.Tag;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.recipe.CookingBookCategory;
+import org.bukkit.inventory.recipe.CraftingBookCategory;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -197,6 +202,292 @@ public final class PylonItems {
                     .set(DataComponentTypes.CONSUMABLE, Consumable.consumable().build())
                     .build()
     );
+
+    public static final PylonItemSchema FERRODURALUM_ORE = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_ore"),
+            new ItemStackBuilder(Material.IRON_ORE)
+                    .name("Ferroduralum ore")
+                    .lore("A crafting material to make",
+                            "armor, weapons and tools.")
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            ferroduralum -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_ore"), ferroduralum);
+                recipe.shape(
+                        "CIR",
+                        "   ",
+                        "   "
+                );
+                recipe.setIngredient('C', Material.COPPER_ORE);
+                recipe.setIngredient('I', Material.IRON_ORE);
+                recipe.setIngredient('R', Material.REDSTONE);
+                recipe.setCategory(CraftingBookCategory.MISC);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_INGOT = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_ingot"),
+            new ItemStackBuilder(Material.IRON_INGOT)
+                    .name("Ferroduralum ingot")
+                    .lore("A crafting material to make",
+                            "armor, weapons and tools.")
+                    .build(),
+            RecipeTypes.VANILLA_FURNACE,
+            ingot -> {
+                FurnaceRecipe recipe = new FurnaceRecipe(pylonKey("ferroduralum_ingot"), ingot, FERRODURALUM_ORE.getItemStack().getType(), 0.25f, 3 * 20);
+                recipe.setCategory(CookingBookCategory.MISC);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_SWORD = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_sword"),
+            new ItemStackBuilder(Material.IRON_SWORD)
+                    .name("Ferroduralum sword")
+                    .lore("A more powerful iron sword.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.ATTACK_DAMAGE, new AttributeModifier(
+                                    pylonKey("ferroduralum_sword_damage"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            sword -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_sword"), sword);
+                recipe.shape(
+                        " F ",
+                        " F ",
+                        " S "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setIngredient('S', Material.STICK);
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_AXE = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_axe"),
+            new ItemStackBuilder(Material.IRON_AXE)
+                    .name("Ferroduralum axe")
+                    .lore("A more powerful iron axe")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.BLOCK_BREAK_SPEED, new AttributeModifier(
+                                    pylonKey("ferroduralum_axe_speed"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            axe -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_axe"), axe);
+                recipe.shape(
+                        "FF ",
+                        "FS ",
+                        " S "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setIngredient('S', Material.STICK);
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_PICKAXE = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_pickaxe"),
+            new ItemStackBuilder(Material.IRON_PICKAXE)
+                    .name("Ferroduralum pickaxe")
+                    .lore("A more powerful iron pickaxe")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.BLOCK_BREAK_SPEED, new AttributeModifier(
+                                    pylonKey("ferroduralum_pickaxe_speed"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            pick -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_pickaxe"), pick);
+                recipe.shape(
+                        "FFF",
+                        " S ",
+                        " S "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setIngredient('S', Material.STICK);
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_SHOVEL = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_shovel"),
+            new ItemStackBuilder(Material.IRON_SHOVEL)
+                    .name("Ferroduralum shovel")
+                    .lore("A more powerful iron shovel.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.BLOCK_BREAK_SPEED, new AttributeModifier(
+                                    pylonKey("ferroduralum_shovel_speed"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            shovel -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_shovel"), shovel);
+                recipe.shape(
+                        " F ",
+                        " S ",
+                        " S "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setIngredient('S', Material.STICK);
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_HOE = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_hoe"),
+            new ItemStackBuilder(Material.IRON_HOE)
+                    .name("Ferroduralum hoe")
+                    .lore("A more powerful iron hoe.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.BLOCK_BREAK_SPEED, new AttributeModifier(
+                                    pylonKey("ferroduralum_hoe_speed"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            hoe -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_hoe"), hoe);
+                recipe.shape(
+                        "FF ",
+                        " S ",
+                        " S "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setIngredient('S', Material.STICK);
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_HELMET = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_helmet"),
+            new ItemStackBuilder(Material.IRON_HELMET)
+                    .name("Ferroduralum helmet")
+                    .lore("A more powerful iron helmet.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.ARMOR, new AttributeModifier(
+                                    pylonKey("ferroduralum_helmet_armor"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            helmet -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_helmet"), helmet);
+                recipe.shape(
+                        "FFF",
+                        "F F",
+                        "   "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_CHESTPLATE = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_chestplate"),
+            new ItemStackBuilder(Material.IRON_CHESTPLATE)
+                    .name("Ferroduralum chestplate")
+                    .lore("A more powerful iron chestplate.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.ARMOR, new AttributeModifier(
+                                    pylonKey("ferroduralum_chestplate_armor"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            chestplate -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_chestplate"), chestplate);
+                recipe.shape(
+                        "F F",
+                        "FFF",
+                        "FFF"
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_LEGGINGS = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_leggings"),
+            new ItemStackBuilder(Material.IRON_LEGGINGS)
+                    .name("Ferroduralum leggings")
+                    .lore("More powerful iron leggings.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.ARMOR, new AttributeModifier(
+                                    pylonKey("ferroduralum_leggings_armor"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            leggings -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_leggings"), leggings);
+                recipe.shape(
+                        "FFF",
+                        "F F",
+                        "F F"
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
+
+    public static final PylonItemSchema FERRODURALUM_BOOTS = new SimpleItemSchema<>(
+            pylonKey("ferroduralum_boots"),
+            new ItemStackBuilder(Material.IRON_BOOTS)
+                    .name("Ferroduralum boots")
+                    .lore("More powerful iron boots.")
+                    .set(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+                            .addModifier(Attribute.ARMOR, new AttributeModifier(
+                                    pylonKey("ferroduralum_boots_armor"),
+                                    1.15,
+                                    AttributeModifier.Operation.MULTIPLY_SCALAR_1
+                            ))
+                            .build())
+                    .build(),
+            RecipeTypes.VANILLA_CRAFTING,
+            boots -> {
+                ShapedRecipe recipe = new ShapedRecipe(pylonKey("ferroduralum_boots"), boots);
+                recipe.shape(
+                        "F F",
+                        "F F",
+                        "   "
+                );
+                recipe.setIngredient('F', FERRODURALUM_INGOT.getItemStack());
+                recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+                return recipe;
+            }
+    );
     //</editor-fold>
 
     static void register() {
@@ -208,6 +499,17 @@ public final class PylonItems {
         DIAMOND_HAMMER.register();
         MONSTER_JERKY.register();
         WATERING_CAN.register();
+        FERRODURALUM_ORE.register();
+        FERRODURALUM_INGOT.register();
+        FERRODURALUM_SWORD.register();
+        FERRODURALUM_AXE.register();
+        FERRODURALUM_PICKAXE.register();
+        FERRODURALUM_SHOVEL.register();
+        FERRODURALUM_HOE.register();
+        FERRODURALUM_HELMET.register();
+        FERRODURALUM_CHESTPLATE.register();
+        FERRODURALUM_LEGGINGS.register();
+        FERRODURALUM_BOOTS.register();
     }
 
     private static @NotNull NamespacedKey pylonKey(@NotNull String key) {
