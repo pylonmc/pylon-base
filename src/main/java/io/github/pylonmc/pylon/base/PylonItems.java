5bcaac618d3957d93cf252e9dd09caa70c6aa8ca
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-03-30 17:27:40 -0400

    decapitalize

--- src/main/java/io/github/pylonmc/pylon/base/PylonItems.java
+++ src/main/java/io/github/pylonmc/pylon/base/PylonItems.java
@@ -719,7 +719,7 @@ public final class PylonItems {
                     .name("Beheading sword")
                     .lore(new LoreBuilder()
                             .arrow().text(" Gives you the heads of what you kill").newline()
-                            .attributeLine("Works with: ", "Players, Creepers, Piglins, Ender dragons and Zombies", Quantity.NONE))
+                            .attributeLine("Works with: ", "players, creepers, piglins, ender dragons and zombies", Quantity.NONE))
                     .set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true)
                     .build()
     );
