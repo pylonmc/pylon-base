04978b818764b09d80b83d20a948c1f40d11be78
Seggan <seggan21@gmail.com>
2025-06-05 11:55:25 -0400

    Build fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
     java
     idea
-    kotlin("jvm") version "1.9.22"
     id("com.gradleup.shadow") version "8.3.2"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     id("xyz.jpenilla.run-paper") version "2.3.0"
@@ -30,7 +29,6 @@ dependencies {
     } else {
         compileOnly("io.github.pylonmc:pylon-core:$coreVersion")
     }
-    implementation("xyz.xenondevs.invui:invui:1.45")
 }
 
 idea {
