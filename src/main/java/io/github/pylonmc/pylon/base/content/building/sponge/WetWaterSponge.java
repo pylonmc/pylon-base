52b150bad65845bdb31dbfac2d826a65ef1c1e68
balugaq <129668183+balugaq@users.noreply.github.com>
2025-08-14 17:32:51 +0800

    Dry out the sponge in the nether

--- src/main/java/io/github/pylonmc/pylon/base/content/building/sponge/WetWaterSponge.java
+++ src/main/java/io/github/pylonmc/pylon/base/content/building/sponge/WetWaterSponge.java
@@ -1,10 +1,20 @@
 package io.github.pylonmc.pylon.base.content.building.sponge;
 
+import io.github.pylonmc.pylon.base.BaseKeys;
+import io.github.pylonmc.pylon.base.util.BaseUtils;
+import io.github.pylonmc.pylon.core.block.BlockStorage;
 import io.github.pylonmc.pylon.core.block.PylonBlock;
+import io.github.pylonmc.pylon.core.block.base.PylonTickingBlock;
 import io.github.pylonmc.pylon.core.item.PylonItem;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * WetWaterSponge is the result of a {@link PowerfulWaterSponge} absorbing water.
@@ -20,6 +30,14 @@ import org.jetbrains.annotations.NotNull;
 public class WetWaterSponge extends PylonBlock {
     protected WetWaterSponge(@NotNull Block block) {
         super(block);
+
+        // Dry out the sponge in the nether
+        Location location = block.getLocation();
+        if (location.getWorld().getEnvironment() == World.Environment.NETHER) {
+            BlockStorage.breakBlock(location);
+            BlockStorage.placeBlock(location, BaseKeys.POWERFUL_WATER_SPONGE);
+            BaseUtils.spawnParticle(Particle.ELECTRIC_SPARK, location.add(0.5, 1, 0.5), 40);
+        }
     }
 
     /**
