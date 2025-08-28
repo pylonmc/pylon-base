aae5a13186d266ebd7556ff8aa2e8e26e480dae8
Seggan <seggan21@gmail.com>
2025-08-28 13:36:44 -0500

    Use Core snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ group = "io.github.pylonmc"
 
 repositories {
     mavenCentral()
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
     maven("https://repo.papermc.io/repository/maven-public/") {
         name = "papermc"
     }
