2f9447adc6dc29ef409920fcb7a0b0d7a51bb8fa
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-08-28 17:05:54 -0400

    fix the thing

---
+++ src/main/java/io/github/pylonmc/pylon/base/content/tools/HealingConsumable.java
@@ -0,0 +1,30 @@
+package io.github.pylonmc.pylon.base.content.tools;
+
+import io.github.pylonmc.pylon.core.i18n.PylonArgument;
+import io.github.pylonmc.pylon.core.item.PylonItem;
+import io.github.pylonmc.pylon.core.item.base.PylonConsumable;
+import io.github.pylonmc.pylon.core.util.gui.unit.UnitFormat;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class HealingConsumable extends PylonItem implements PylonConsumable {
+    private final double consumeSeconds = getSettings().getOrThrow("consume-seconds", Double.class);
+    private final double healAmount = getSettings().getOrThrow("heal-amount", Double.class);
+    public HealingConsumable(@NotNull ItemStack stack){ super(stack); }
+
+    @Override
+    public @NotNull List<PylonArgument> getPlaceholders() {
+        return List.of(
+                PylonArgument.of("consume-seconds", UnitFormat.SECONDS.format(consumeSeconds)),
+                PylonArgument.of("heal-amount", UnitFormat.HEARTS.format(healAmount))
+        );
+    }
+
+    @Override
+    public void onConsumed(@NotNull PlayerItemConsumeEvent event) {
+        event.getPlayer().heal(healAmount);
+    }
+}
