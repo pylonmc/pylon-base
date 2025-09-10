139b1089a56a64c74a7fc6026951edb57c1d3b1b
Seggan <seggan21@gmail.com>
2025-09-10 15:17:31 -0400

    Table saw

--- src/main/java/io/github/pylonmc/pylon/base/BaseRecipes.java
+++ src/main/java/io/github/pylonmc/pylon/base/BaseRecipes.java
@@ -1,8 +1,6 @@
 package io.github.pylonmc.pylon.base;
 
 import io.github.pylonmc.pylon.base.recipes.*;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 public class BaseRecipes {
 
@@ -38,44 +36,5 @@ public class BaseRecipes {
         StrainingRecipe.RECIPE_TYPE.register();
 
         TableSawRecipe.RECIPE_TYPE.register();
-
-        addPlanksRecipe(Material.OAK_LOG, Material.OAK_PLANKS);
-        addPlanksRecipe(Material.SPRUCE_LOG, Material.SPRUCE_PLANKS);
-        addPlanksRecipe(Material.BIRCH_LOG, Material.BIRCH_PLANKS);
-        addPlanksRecipe(Material.JUNGLE_LOG, Material.JUNGLE_PLANKS);
-        addPlanksRecipe(Material.ACACIA_LOG, Material.ACACIA_PLANKS);
-        addPlanksRecipe(Material.CHERRY_LOG, Material.CHERRY_PLANKS);
-        addPlanksRecipe(Material.PALE_OAK_LOG, Material.PALE_OAK_PLANKS);
-        addPlanksRecipe(Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS);
-        addPlanksRecipe(Material.MANGROVE_LOG, Material.MANGROVE_PLANKS);
-        addStickRecipe(Material.OAK_PLANKS);
-        addStickRecipe(Material.SPRUCE_PLANKS);
-        addStickRecipe(Material.BIRCH_PLANKS);
-        addStickRecipe(Material.JUNGLE_PLANKS);
-        addStickRecipe(Material.ACACIA_PLANKS);
-        addStickRecipe(Material.CHERRY_PLANKS);
-        addStickRecipe(Material.PALE_OAK_PLANKS);
-        addStickRecipe(Material.DARK_OAK_PLANKS);
-        addStickRecipe(Material.MANGROVE_PLANKS);
-    }
-
-    private static void addPlanksRecipe(Material input, Material output) {
-        TableSawRecipe.RECIPE_TYPE.addRecipe(new TableSawRecipe(
-                input.getKey(),
-                new ItemStack(input),
-                new ItemStack(output, 6),
-                input.createBlockData(),
-                20
-        ));
-    }
-
-    private static void addStickRecipe(Material input) {
-        TableSawRecipe.RECIPE_TYPE.addRecipe(new TableSawRecipe(
-                input.getKey(),
-                new ItemStack(input),
-                new ItemStack(Material.STICK, 6),
-                input.createBlockData(),
-                20
-        ));
     }
 }
