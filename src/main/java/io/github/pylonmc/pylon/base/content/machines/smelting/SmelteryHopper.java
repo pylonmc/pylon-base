5f04907037acd2066993bad3e192c39f48f8d471
LordIdra <percy.baker.0@gmail.com>
2025-09-17 15:00:36 +0100

    Fix smeltery hopper still consuming items when smeltery full

--- src/main/java/io/github/pylonmc/pylon/base/content/machines/smelting/SmelteryHopper.java
+++ src/main/java/io/github/pylonmc/pylon/base/content/machines/smelting/SmelteryHopper.java
@@ -49,7 +49,8 @@ public final class SmelteryHopper extends SmelteryComponent implements PylonTick
                 }
             }
             if (recipe == null) continue;
-            if (controller.getTemperature() >= recipe.temperature()) {
+            double fluidAmountAfterAdding = controller.getTotalFluid() + recipe.resultAmount();
+            if (controller.getTemperature() >= recipe.temperature() && fluidAmountAfterAdding <= controller.getCapacity()) {
                 controller.addFluid(recipe.result(), recipe.resultAmount());
                 item.subtract();
                 new PylonCraftEvent<>(MeltingRecipe.RECIPE_TYPE, recipe, controller).callEvent();
