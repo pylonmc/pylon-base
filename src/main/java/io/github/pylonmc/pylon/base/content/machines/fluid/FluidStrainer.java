9c7076a395df50b2fcfcfdb9bfd8a12058cf4aa6
LordIdra <percy.baker.0@gmail.com>
2025-09-17 18:39:04 +0100

    pr comment

--- src/main/java/io/github/pylonmc/pylon/base/content/machines/fluid/FluidStrainer.java
+++ src/main/java/io/github/pylonmc/pylon/base/content/machines/fluid/FluidStrainer.java
@@ -133,7 +133,7 @@ public class FluidStrainer extends PylonBlock
     @Override
     @NotNull
     public Component getGuiTitle() {
-        return Component.translatable("pylon.pylonbase.item.fluid_strainer.name");
+        return getDefaultTranslationKey();
     }
 
     private final VirtualInventory inventory = new VirtualInventory(9 * 3);
