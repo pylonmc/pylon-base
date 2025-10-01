5f14609741ed5ac02eba146b0edd40ae97164cbf
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-10-01 02:36:56 -0500

    fluid tank update

--- src/main/java/io/github/pylonmc/pylon/base/content/machines/fluid/FluidTank.java
+++ src/main/java/io/github/pylonmc/pylon/base/content/machines/fluid/FluidTank.java
@@ -9,8 +9,6 @@ import io.github.pylonmc.pylon.core.block.context.BlockCreateContext;
 import io.github.pylonmc.pylon.core.waila.WailaDisplay;
 import io.github.pylonmc.pylon.core.config.adapter.ConfigAdapter;
 import io.github.pylonmc.pylon.core.content.fluid.FluidPointInteraction;
-import io.github.pylonmc.pylon.core.datatypes.EnumPersistentDataType;
-import io.github.pylonmc.pylon.core.datatypes.PylonSerializers;
 import io.github.pylonmc.pylon.core.entity.display.ItemDisplayBuilder;
 import io.github.pylonmc.pylon.core.entity.display.transform.TransformBuilder;
 import io.github.pylonmc.pylon.core.fluid.FluidPointType;
@@ -20,24 +18,22 @@ import io.github.pylonmc.pylon.core.i18n.PylonArgument;
 import io.github.pylonmc.pylon.core.item.PylonItem;
 import io.github.pylonmc.pylon.core.util.gui.unit.UnitFormat;
 import io.github.pylonmc.pylon.core.util.position.ChunkPosition;
+import io.papermc.paper.datacomponent.DataComponentTypes;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.Style;
-import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
-import static io.github.pylonmc.pylon.base.util.BaseUtils.baseKey;
 
 public class FluidTank extends PylonBlock
         implements PylonMultiblock, PylonFluidTank, PylonEntityHolderBlock {
@@ -58,21 +54,16 @@ public class FluidTank extends PylonBlock
         }
     }
 
-    private static final NamespacedKey HEIGHT_KEY = baseKey("height");
-    private static final NamespacedKey ALLOWED_TEMPERATURES_KEY = baseKey("allowed_temperatures");
-    private static final PersistentDataType<List<String>, List<FluidTemperature>> ALLOWED_TEMPERATURES_TYPE
-            = PylonSerializers.LIST.listTypeFrom(new EnumPersistentDataType<>(FluidTemperature.class));
-
     private final int maxHeight = getSettings().getOrThrow("max-height", ConfigAdapter.INT);
 
-    private int height;
-    private List<FluidTemperature> allowedTemperatures;
+    private final List<FluidTankCasing> casings = new ArrayList<>();
+    private final List<FluidTemperature> allowedTemperatures = new ArrayList<>();
+
+    private int lastDisplayUpdate = -1;
 
     @SuppressWarnings("unused")
     public FluidTank(@NotNull Block block, @NotNull BlockCreateContext context) {
         super(block, context);
-        height = 0;
-        allowedTemperatures = List.of();
         addEntity("fluid", new ItemDisplayBuilder()
                 .build(getBlock().getLocation().toCenterLocation().add(0, 1, 0))
         );
@@ -83,14 +74,6 @@ public class FluidTank extends PylonBlock
     @SuppressWarnings({"unused", "DataFlowIssue"})
     public FluidTank(@NotNull Block block, @NotNull PersistentDataContainer pdc) {
         super(block, pdc);
-        height = pdc.get(HEIGHT_KEY, PylonSerializers.INTEGER);
-        allowedTemperatures = pdc.get(ALLOWED_TEMPERATURES_KEY, ALLOWED_TEMPERATURES_TYPE);
-    }
-
-    @Override
-    public void write(@NotNull PersistentDataContainer pdc) {
-        pdc.set(HEIGHT_KEY, PylonSerializers.INTEGER, height);
-        pdc.set(ALLOWED_TEMPERATURES_KEY, ALLOWED_TEMPERATURES_TYPE, allowedTemperatures);
     }
 
     @Override
@@ -100,38 +83,63 @@ public class FluidTank extends PylonBlock
 
     @Override
     public boolean checkFormed() {
-        height = 0;
-        FluidTankCasing casingType = null;
+        casings.forEach(FluidTankCasing::reset);
+        casings.clear();
         for (int i = 0; i < maxHeight; i++) {
             FluidTankCasing casing = BlockStorage.getAs(
                     FluidTankCasing.class,
-                    getBlock().getLocation().add(0, i + 1, 0)
+                    getBlock().getRelative(0, i + 1, 0)
             );
-            if (casingType == null) {
-                casingType = casing;
-            }
+
+            FluidTankCasing casingType = casings.isEmpty() ? casing : casings.getFirst();
             if (casing == null || casing.getKey() != casingType.getKey()) {
                 break;
             }
-            height++;
-        }
 
-        if (casingType != null) {
-            allowedTemperatures = casingType.getAllowedTemperatures();
-            setCapacity(height * casingType.getCapacity());
-        } else {
-            allowedTemperatures = List.of();
-            setCapacity(0);
+            casing.setFluidTank(this);
+            casings.add(casing);
         }
+        return !casings.isEmpty();
+    }
+
+    @Override
+    public void onMultiblockFormed() {
+        onMultiblockRefreshed();
+    }
+
+    @Override
+    public void onMultiblockRefreshed() {
+        FluidTankCasing casingType = casings.getFirst();
+        allowedTemperatures.clear();
+        allowedTemperatures.addAll(casingType.getAllowedTemperatures());
+        setCapacity(casings.size() * casingType.getCapacity());
         setFluid(Math.min(getFluidCapacity(), getFluidAmount()));
 
-        return casingType != null;
+        int height = casings.size();
+        for (int i = 0; i < casings.size(); i++) {
+            FluidTankCasing casing = casings.get(i);
+            if (i == 0) {
+                casing.setShape(height == 1 ? "single" : "bottom");
+            } else if (i == casings.size() - 1) {
+                casing.setShape("top");
+            } else {
+                casing.setShape("middle");
+            }
+        }
+    }
+
+    @Override
+    public void onMultiblockUnformed() {
+        casings.forEach(casing -> casing.setFluidTank(null));
+        casings.clear();
+        allowedTemperatures.clear();
+        setCapacity(0);
+        setFluid(0);
     }
 
     @Override
     public boolean isPartOfMultiblock(@NotNull Block otherBlock) {
-        Vector offset = otherBlock.getLocation().toVector()
-                .subtract(getBlock().getLocation().toVector());
+        Vector offset = otherBlock.getLocation().toVector().subtract(getBlock().getLocation().toVector());
         return offset.getBlockX() == 0
                 && offset.getBlockY() > 0
                 && offset.getBlockY() <= maxHeight
@@ -140,8 +148,7 @@ public class FluidTank extends PylonBlock
 
     @Override
     public boolean isAllowedFluid(@NotNull PylonFluid fluid) {
-        return fluid.hasTag(FluidTemperature.class)
-                && allowedTemperatures.contains(fluid.getTag(FluidTemperature.class));
+        return fluid.hasTag(FluidTemperature.class) && allowedTemperatures.contains(fluid.getTag(FluidTemperature.class));
     }
 
     @Override
@@ -152,13 +159,21 @@ public class FluidTank extends PylonBlock
 
     @Override
     public boolean setFluid(double amount) {
+        double oldAmount = getFluidAmount();
         boolean result = PylonFluidTank.super.setFluid(amount);
-        float scale = (float) ((height - 0.1) * getFluidAmount() / getFluidCapacity());
-        getFluidDisplay().setTransformationMatrix(new TransformBuilder()
-                .translate(0.0, -0.45 + scale / 2, 0.0)
-                .scale(0.9, scale, 0.9)
-                .buildForItemDisplay()
-        );
+        amount = getFluidAmount();
+        if (lastDisplayUpdate == -1 || (result && oldAmount != amount)) {
+            float scale = (float) ((casings.size() - 0.1) * amount / getFluidCapacity());
+            ItemDisplay fluidDisplay = getFluidDisplay();
+            fluidDisplay.setInterpolationDelay(Math.min(-3 + (fluidDisplay.getTicksLived() - lastDisplayUpdate), 0));
+            fluidDisplay.setInterpolationDuration(4);
+            fluidDisplay.setTransformationMatrix(new TransformBuilder()
+                    .translate(0.0, -0.45 + scale / 2, 0.0)
+                    .scale(0.9, scale, 0.9)
+                    .buildForItemDisplay()
+            );
+            lastDisplayUpdate = fluidDisplay.getTicksLived();
+        }
         return result;
     }
 
