1eb2b4080127f9f285cd8f722e51f79310761c32
Seggan <seggan21@gmail.com>
2025-10-02 20:20:53 -0400

    Finish steel smelting

--- src/main/java/io/github/pylonmc/pylon/base/content/machines/smelting/BronzeAnvil.java
+++ src/main/java/io/github/pylonmc/pylon/base/content/machines/smelting/BronzeAnvil.java
@@ -1,5 +1,8 @@
 package io.github.pylonmc.pylon.base.content.machines.smelting;
 
+import io.github.pylonmc.pylon.base.BaseItems;
+import io.github.pylonmc.pylon.base.content.resources.Bloom;
+import io.github.pylonmc.pylon.base.content.tools.Hammer;
 import io.github.pylonmc.pylon.base.entities.SimpleItemDisplay;
 import io.github.pylonmc.pylon.core.block.PylonBlock;
 import io.github.pylonmc.pylon.core.block.base.PylonEntityHolderBlock;
@@ -10,12 +13,15 @@ import io.github.pylonmc.pylon.core.block.context.BlockCreateContext;
 import io.github.pylonmc.pylon.core.config.adapter.ConfigAdapter;
 import io.github.pylonmc.pylon.core.entity.display.ItemDisplayBuilder;
 import io.github.pylonmc.pylon.core.entity.display.transform.TransformBuilder;
+import io.github.pylonmc.pylon.core.item.PylonItem;
+import io.github.pylonmc.pylon.core.particles.PylonParticleBuilder;
+import io.github.pylonmc.pylon.core.util.PylonUtils;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Directional;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
@@ -24,17 +30,18 @@ import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class BronzeAnvil extends PylonBlock implements PylonEntityHolderBlock, PylonTickingBlock, PylonInteractBlock {
 
     public final int tickInterval = getSettings().getOrThrow("tick-interval", ConfigAdapter.INT);
+    public final float coolChance = getSettings().getOrThrow("cool-chance", ConfigAdapter.FLOAT);
 
     private static final Matrix4f BASE_TRANSFORM = new TransformBuilder()
             .scale(0.3)
             .translate(0, (1 - .5 + 1d / 16) * 3, 0)
             .rotate(Math.PI / 2, 0, 0)
-            .buildForItemDisplay()
-            .rotateLocalY((float) (Math.PI / 2));
+            .buildForItemDisplay();
 
     @SuppressWarnings("unused")
     public BronzeAnvil(@NotNull Block block, @NotNull BlockCreateContext context) {
@@ -64,8 +71,15 @@ public final class BronzeAnvil extends PylonBlock implements PylonEntityHolderBl
 
     @Override
     public void onInteract(@NotNull PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getHand() != EquipmentSlot.HAND) return;
-        event.setCancelled(true);
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getAction().isRightClick()) {
+            onRightClick(event);
+        } else if (event.getAction().isLeftClick()) {
+            onLeftClick(event);
+        }
+    }
+
+    private void onRightClick(@NotNull PlayerInteractEvent event) {
         //noinspection DuplicatedCode
         ItemStack placedItem = event.getItem();
 
@@ -75,6 +89,9 @@ public final class BronzeAnvil extends PylonBlock implements PylonEntityHolderBl
             if (placedItem != null) {
                 itemDisplay.setItemStack(placedItem.asOne());
                 placedItem.subtract();
+                if (PylonItem.fromStack(itemDisplay.getItemStack()) instanceof Bloom bloom) {
+                    transformForWorking(bloom.getWorking());
+                }
             }
         } else {
             Player player = event.getPlayer();
@@ -83,24 +100,109 @@ public final class BronzeAnvil extends PylonBlock implements PylonEntityHolderBl
             }
             itemDisplay.setItemStack(null);
         }
+        event.setCancelled(true);
+        event.getPlayer().swingHand(EquipmentSlot.HAND);
+    }
+
+    private void onLeftClick(@NotNull PlayerInteractEvent event) {
+        ItemDisplay itemDisplay = getItemDisplay();
+        if (!(PylonItem.fromStack(itemDisplay.getItemStack()) instanceof Bloom bloom)) return;
+
+        ItemStack item = event.getItem();
+        if (item == null || item.getType().isAir()) return;
+
+        int temperature = bloom.getTemperature();
+
+        int workingChange = ThreadLocalRandom.current().nextInt(-1, 2);
+        if (temperature == 0) {
+            workingChange = 0;
+        } else if (PylonUtils.isPylonSimilar(item, BaseItems.TONGS)) {
+            workingChange -= temperature;
+        } else if (PylonItem.fromStack(item) instanceof Hammer hammer && !event.getPlayer().hasCooldown(item)) {
+            workingChange += temperature;
+            event.getPlayer().setCooldown(item, hammer.cooldownTicks);
+        } else {
+            return;
+        }
+
+        event.setCancelled(true);
+        event.getPlayer().swingHand(EquipmentSlot.HAND);
+
+        int working = bloom.getWorking();
+        int newWorking = working + workingChange;
+        Location centerLoc = getBlock().getRelative(BlockFace.UP).getLocation().toCenterLocation();
+        if (Math.abs(working) > Bloom.MAX_WORKING) {
+            new PylonParticleBuilder.Type.Item()
+                    .location(centerLoc)
+                    .receivers(32, true)
+                    .velocity(0.3, 0.1, 0.3)
+                    .item(bloom.getStack())
+                    .multiple()
+                    .count(8)
+                    .spawn();
+            itemDisplay.setItemStack(null);
+            return;
+        } else if (Math.abs(newWorking) < Math.abs(working)) {
+            new PylonParticleBuilder.Type.SmallFlame()
+                    .location(centerLoc)
+                    .receivers(32, true)
+                    .velocity(0.3, 0.1, 0.3)
+                    .multiple()
+                    .count(temperature)
+                    .spawn();
+        } else {
+            new PylonParticleBuilder.Type.ElectricSpark()
+                    .location(centerLoc)
+                    .receivers(32, true)
+                    .velocity(0.2, 0.1, 0.2)
+                    .multiple()
+                    .count(temperature)
+                    .spawn();
+        }
+        bloom.setWorking(newWorking);
+        itemDisplay.setItemStack(bloom.getStack());
+        transformForWorking(newWorking);
     }
 
     @Override
     public void tick(double deltaSeconds) {
-
+        if (ThreadLocalRandom.current().nextFloat() > coolChance) return;
+        ItemDisplay itemDisplay = getItemDisplay();
+        if (!(PylonItem.fromStack(itemDisplay.getItemStack()) instanceof Bloom bloom)) return;
+        int newTemperature = Math.max(0, bloom.getTemperature() - 1);
+        bloom.setTemperature(newTemperature);
+        if (bloom.getWorking() == 0 && newTemperature == 0) {
+            itemDisplay.setItemStack(null);
+            Location centerLoc = getBlock().getLocation().toCenterLocation();
+            centerLoc.getWorld().dropItemNaturally(centerLoc, BaseItems.WROUGHT_IRON.clone());
+            itemDisplay.setItemStack(null);
+            return;
+        }
+        itemDisplay.setItemStack(bloom.getStack());
     }
 
     public @NotNull ItemDisplay getItemDisplay() {
         return getHeldEntityOrThrow(SimpleItemDisplay.class, "item").getEntity();
     }
 
+    private void transformForWorking(int working) {
+        Matrix4f transform = new Matrix4f(BASE_TRANSFORM)
+                .rotateLocalY(getItemRotation())
+                .scaleLocal(
+                        Math.max(0, working * 0.5f) + 1,
+                        1,
+                        Math.max(0, -working * 0.5f) + 1
+                );
+        getItemDisplay().setTransformationMatrix(transform);
+    }
+
     private float getItemRotation() {
         BlockFace orientation = ((Directional) getBlock().getBlockData()).getFacing();
         return (float) switch (orientation) {
-            case NORTH -> 0;
-            case EAST -> 3 * Math.PI / 2;
-            case SOUTH -> Math.PI;
-            case WEST -> Math.PI / 2;
+            case NORTH -> 3 * Math.PI / 2;
+            case EAST -> Math.PI;
+            case SOUTH -> Math.PI / 2;
+            case WEST -> 0;
             default -> 0;
         };
     }
