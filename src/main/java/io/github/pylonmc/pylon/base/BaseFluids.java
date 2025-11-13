1b41bb8593607803d2b1c5856509f6426390f1ad
Seggan <seggan21@gmail.com>
2025-11-13 16:10:49 -0500

    Various recipe-related things
    Resolves #274
    Resolves #419
    Fixes #252
    Resolves #396

--- src/main/java/io/github/pylonmc/pylon/base/BaseFluids.java
+++ src/main/java/io/github/pylonmc/pylon/base/BaseFluids.java
@@ -1,15 +1,10 @@
 package io.github.pylonmc.pylon.base;
 
 import io.github.pylonmc.pylon.base.content.machines.smelting.Slurry;
-import io.github.pylonmc.pylon.base.recipes.CastingRecipe;
-import io.github.pylonmc.pylon.base.recipes.MeltingRecipe;
 import io.github.pylonmc.pylon.core.fluid.PylonFluid;
 import io.github.pylonmc.pylon.core.fluid.tags.FluidTemperature;
-import io.github.pylonmc.pylon.core.recipe.RecipeInput;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.Nullable;
 
 import static io.github.pylonmc.pylon.base.util.BaseUtils.baseKey;
 
@@ -153,169 +148,32 @@ public final class BaseFluids {
     // TODO refactor into static blocks as in BaseItems
     public static void initialize() {
         WATER.register();
-
         LAVA.register();
-
         OBSCYRA.register();
-
         SULFUR.register();
-
         MERCURY.register();
-
         COPPER.register();
-        addMetalRecipes(
-                COPPER,
-                1083,
-                new ItemStack(Material.COPPER_INGOT),
-                BaseItems.COPPER_DUST,
-                null,
-                new ItemStack(Material.COPPER_BLOCK)
-        );
-
         GOLD.register();
-        addMetalRecipes(
-                GOLD,
-                1064,
-                new ItemStack(Material.GOLD_INGOT),
-                BaseItems.GOLD_DUST,
-                null,
-                new ItemStack(Material.GOLD_BLOCK)
-        );
-
         IRON.register();
-        addMetalRecipes(
-                IRON,
-                1064,
-                new ItemStack(Material.IRON_INGOT),
-                BaseItems.IRON_DUST,
-                new ItemStack(Material.IRON_NUGGET),
-                new ItemStack(Material.IRON_BLOCK)
-        );
-
         TIN.register();
-        addMetalRecipes(
-                TIN,
-                231.9,
-                BaseItems.TIN_INGOT,
-                BaseItems.TIN_DUST,
-                BaseItems.TIN_NUGGET,
-                BaseItems.TIN_BLOCK
-        );
-
         COBALT.register();
-        addMetalRecipes(
-                COBALT,
-                1495,
-                BaseItems.COBALT_INGOT,
-                BaseItems.COBALT_DUST,
-                BaseItems.COBALT_NUGGET,
-                BaseItems.COBALT_BLOCK
-        );
-
         NICKEL.register();
-        addMetalRecipes(
-                NICKEL,
-                1455,
-                BaseItems.NICKEL_INGOT,
-                BaseItems.NICKEL_DUST,
-                BaseItems.NICKEL_NUGGET,
-                BaseItems.NICKEL_BLOCK
-        );
-
         BRONZE.register();
-        addMetalRecipes(
-                BRONZE,
-                950,
-                BaseItems.BRONZE_INGOT,
-                BaseItems.BRONZE_DUST,
-                BaseItems.BRONZE_NUGGET,
-                BaseItems.BRONZE_BLOCK
-        );
-
         STEEL.register();
-        addMetalRecipes(
-                STEEL,
-                950,
-                BaseItems.STEEL_INGOT,
-                BaseItems.STEEL_DUST,
-                BaseItems.STEEL_NUGGET,
-                BaseItems.STEEL_BLOCK
-        );
 
         SLURRY.register();
-
         COAL_SLURRY.register();
-
         CARBON_SLURRY.register();
-
         RAW_COPPER_SLURRY.register();
-
         RAW_GOLD_SLURRY.register();
-
         RAW_IRON_SLURRY.register();
-
         RAW_TIN_SLURRY.register();
-
         REDSTONE_SLURRY.register();
 
         PLANT_OIL.register();
-
         HYDRAULIC_FLUID.register();
-
         DIRTY_HYDRAULIC_FLUID.register();
-
         REFLECTOR_FLUID.register();
-
         BIODIESEL.register();
-    }
-
-    private static void addMetalRecipes(
-            PylonFluid fluid,
-            double temperature,
-            ItemStack ingot,
-            ItemStack dust,
-            @Nullable ItemStack nugget,
-            ItemStack block
-    ) {
-        CastingRecipe.RECIPE_TYPE.addRecipe(new CastingRecipe(
-                NamespacedKey.fromString(fluid.getKey() + "_to_ingot"),
-                RecipeInput.of(fluid, 144.0),
-                ingot,
-                temperature
-        ));
-
-        MeltingRecipe.RECIPE_TYPE.addRecipe(new MeltingRecipe(
-                NamespacedKey.fromString(fluid.getKey() + "_from_ingot"),
-                RecipeInput.of(ingot),
-                fluid,
-                144.0,
-                temperature
-        ));
-
-        MeltingRecipe.RECIPE_TYPE.addRecipe(new MeltingRecipe(
-                NamespacedKey.fromString(fluid.getKey() + "_from_dust"),
-                RecipeInput.of(dust),
-                fluid,
-                144.0,
-                temperature
-        ));
-
-        if (nugget != null) {
-            MeltingRecipe.RECIPE_TYPE.addRecipe(new MeltingRecipe(
-                    NamespacedKey.fromString(fluid.getKey() + "_from_nugget"),
-                    RecipeInput.of(nugget),
-                    fluid,
-                    16.0,
-                    temperature
-            ));
-        }
-
-        MeltingRecipe.RECIPE_TYPE.addRecipe(new MeltingRecipe(
-                NamespacedKey.fromString(fluid.getKey() + "_from_block"),
-                RecipeInput.of(block),
-                fluid,
-                1296.0,
-                temperature
-        ));
     }
 }
